3e0264a01c17e47e8c547f71287e9789e0a44a3a
brunosntos <brunoucn@gmail.com>
2026-01-03 18:10:21 -0300

    endpoint receber transação

---
+++ app/app.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+from datetime import datetime
+
+from flask import Flask, request, jsonify, Response
+
+app = Flask(__name__)
+
+db = []
+
+@app.route("/transacao", methods=["POST"])
+def criar_transacao():
+    try:
+        dados = request.get_json()
+
+        valor = dados.get("valor", "")
+        dataHora = dados.get("dataHora", "")
+
+        if str(valor).strip() == "" or str(dataHora).strip() == "":
+            return Response(status=422)
+        
+        valor_decimal = Decimal(valor)
+        if valor_decimal < 0:
+            return Response(status=422)
+        
+        dataHora_str = str(dataHora)
+        dataHora_datetime = datetime.fromisoformat(dataHora_str)
+        agora = datetime.now().isoformat()
+        agora_datetime = datetime.fromisoformat(agora)
+
+        if (dataHora_datetime > agora_datetime):
+            return Response(status=422)
+        
+        dados["dataHora"] = dataHora_datetime
+        dados["valor"] = valor_decimal
+
+        db.append(dados)
+        return Response(status=201)
+    
+    except Exception:
+        return Response(status=400)
+
+if __name__ == "__main__":
+    app.run(debug=True)
